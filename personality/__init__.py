506b568e8bdcb5684d22c68ed2334d65d9a2e7ac
Ethan36Z <zhangethan36@gmail.com>
2026-09-05 12:03:40 -0700

    Add Corvus personality runtime package

---
+++ personality/__init__.py
@@ -0,0 +1,9 @@
+from personality.runtime import (
+    PERSONALITY_SPEC_VERSION,
+    compile_personality_system_prompt,
+)
+
+__all__ = [
+    "PERSONALITY_SPEC_VERSION",
+    "compile_personality_system_prompt",
+]
